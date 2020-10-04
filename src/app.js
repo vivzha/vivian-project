2065cebfea5b32b9c0c6f5e78a0c4d3250a042c6
vivzha <vzha215@aucklanduni.ac.nz>
2020-10-04 21:24:55 +1300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "1177d1f3b0df9e5dd2c9f2e91d6cae8d";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1177d1f3b0df9e5dd2c9f2e91d6cae8d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function shandleSubmit(event) {
+  event.preventDefault();
+
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
